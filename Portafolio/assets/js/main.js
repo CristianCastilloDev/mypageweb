34a83de1dc13f9219413ac0e449bfd3087dc7ee1
ErringCougar <110920550+ErringCougar@users.noreply.github.com>
2023-05-31 17:50:40 -0700

    first commit

---
+++ Portafolio/assets/js/main.js
@@ -0,0 +1,45 @@
+/* ============== [ MENU SHOW AND HIDDEN ] ============== */
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose  = document.getElementById('nav-close')
+
+/* ====== [ MENU SHOW ] ====== */
+/* Validate if constant exist */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+/* ====== [ MENU HIDDEN ] ====== */
+/* Validate if constant exist */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+/* ============== [ REMOVE MENU MOBILE ] ============== */
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/* ============== [ ACCORDION SKILLS ] ============== */
+const skillsContent = document.getElementsByClassName('skills_content'),
+    skillsHeader = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
